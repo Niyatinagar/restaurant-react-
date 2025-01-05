db161ebda71b1df7b80b5615d319460ade8e003b
Niyatinagar <166269349+Niyatinagar@users.noreply.github.com>
2025-01-05 13:58:36 +0530

    Add files via upload

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <>
+      <section className="about" id="about">
+        <div className="container">
+          <div className="banner">
+            <div className="top">
+              <h1 className="heading">ABOUT US</h1>
+              <p>The only thing we're serious about is food.</p>
+            </div>
+            <p className="mid">
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Provident possimus optio adipisci dolores impedit illum iusto
+              perferendis, laudantium quod accusamus consequuntur consectetur,
+              tempore nulla error iure reiciendis dolorem assumenda.
+              Necessitatibus fugit asperiores totam rem esse exercitationem
+              iusto ipsum qui dolore ex, accusantium repellat mollitia
+              repellendus.
+            </p>
+          </div>
+          <div className="banner">
+            <img src="about.png" alt="about" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
